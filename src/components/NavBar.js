fdb2385d49e22ad3c9a9bcfa2f8d7baa3fe6685a
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-05-02 18:07:43 +0200

    Adjusting NavBar for loggedIn/loggedOut

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,148 @@
-import React, { useContext } from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from "../assets/logo.png";
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import logo from "../assets/ccc-logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { CurrentUserContext } from "../App";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+import { removeTokenTimestamp } from "../utils/utils";
 
 const NavBar = () => {
-  const currentUser = useContext(CurrentUserContext);
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+      removeTokenTimestamp();
+    } catch (err) {
+    }
+  };
+
+  const addBookReviewIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/book_reviews/create"
+    >
+      <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>New Review</Tooltip>}
+            >
+              <i className="far fa-plus-square"></i>
+            </OverlayTrigger>
+    </NavLink>
+  );
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>Feed</Tooltip>}
+            >
+              <i className="fas fa-stream"></i>
+            </OverlayTrigger>
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/likes"
+      >
+         
+        <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>Liked</Tooltip>}
+            >
+              <i className="fas fa-heart"></i>
+            </OverlayTrigger>
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+         
+        <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>Log-out</Tooltip>}
+            >
+              <i className="fas fa-sign-out-alt"></i>
+            </OverlayTrigger>
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>Profile</Tooltip>}
+            >
+              <i class="fa-solid fa-user"></i>
+            </OverlayTrigger>
+      </NavLink>
+    </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink
         className={styles.NavLink}
         activeClassName={styles.Active}
-        to="/signin"
+        to="/login"
       >
-        <i className="fas fa-sign-in-alt"></i>Sign in
+        <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>Log-in</Tooltip>}
+            >
+              <i className="fas fa-sign-in-alt"></i>
+            </OverlayTrigger>
       </NavLink>
       <NavLink
-        to="/signup"
+        to="/register"
         className={styles.NavLink}
         activeClassName={styles.Active}
       >
-        <i className="fas fa-user-plus"></i>Sign up
+         
+        <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>Register</Tooltip>}
+            >
+              <i className="fas fa-user-plus"></i>
+            </OverlayTrigger>
       </NavLink>
     </>
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
-      <Container>
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
+     <Container>
         <NavLink to="/">
           <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        {currentUser && addBookReviewIcon}
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
+       <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
               exact
@@ -46,7 +150,13 @@ const NavBar = () => {
               activeClassName={styles.Active}
               to="/"
             >
-              <i className="fas fa-home"></i>Home
+              
+              <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>Home</Tooltip>}
+            >
+              <i className="fas fa-home"></i> 
+            </OverlayTrigger>
             </NavLink>
 
             {currentUser ? loggedInIcons : loggedOutIcons}
